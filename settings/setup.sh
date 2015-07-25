dab328e38c8cd67e720fe274fb78952dfb6e2f3e
Amuro Nishizawa <aburos.d@gmail.com>
2015-07-25 23:37:55 +0900

    add create golang environments script for raspberrypi.

--- settings/setup.sh
+++ settings/setup.sh
@@ -5,6 +5,18 @@ pacman -S bluez bluez-utils python python-pip --noconfirm
 systemctl start bluetooth.service
 systemctl enable bluetooth.service
 
-pacman -S openssh --noconfirm
-systemctl start sshd.service
-systemctl enable sshd.service
+pacman -S wget git mercurial go --noconfirm
+
+mkdir ~/gocode
+echo GOPATH=~/gocode >> ~/.bashrc
+echo GOPATH=~/gocode >> ~/.bash_profile
+echo PATH=$PATH:$GOCODE/bin >> ~/.bashrc
+echo PATH=$PATH:$GOCODE/bin >> ~/.bash_profile
+
+go get github.com/revel/revel
+go get github.com/revel/cmd/revel
+go get github.com/stianeikeland/go-rpio
+
+go version
+revel
+
